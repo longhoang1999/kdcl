3ea719351a89616bae1b82f027b557492881d918
Namlim02 <namljm02@gmail.com>
2023-08-01 10:23:20 +0700

    Sua 2 bang cu

--- app/Http/Controllers/Admin/Project/Importdata2/Tuyensinh/TuyensinhController.php
+++ app/Http/Controllers/Admin/Project/Importdata2/Tuyensinh/TuyensinhController.php
@@ -38,18 +38,24 @@ class TuyensinhController extends DefinedController{
         $getFile = DB::table('excel_import_data2')->where('type_excel', '1')->select("id", "year")->get();
 		
         if(Sentinel::inRole('truongdonvi')){
+    
             $phanquen = DB::table('lkh_phanquyen_excel')
                             ->where('bang_stt',1)
                             ->first();
-            if($phanquen->donvi_id == Sentinel::getUser()->donvi_id){
+            if($phanquen){
+                if($phanquen->donvi_id == Sentinel::getUser()->donvi_id){
                     return view('admin.project.Importdata2.admissions')->with([
                         'loai_dv'           => $loai_dv,
                         'donvi'             => $donvi,
                         'getFile'           => $getFile
                     ]);
+                }else{
+                    return redirect()->back()->withErrors("");
+                }
             }else{
-                return redirect()->back();
+                return redirect()->back()->withErrors(""); 
             }
+            
 
         }
 
