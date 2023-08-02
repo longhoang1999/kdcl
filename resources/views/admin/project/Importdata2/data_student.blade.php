9f5cd724f6681515e08e9e57739751bbddfde54a
longhoang1999 <shikatori1999@gmail.com>
2023-08-02 20:15:03 +0700

    update quyen

--- resources/views/admin/project/Importdata2/data_student.blade.php
+++ resources/views/admin/project/Importdata2/data_student.blade.php
@@ -78,6 +78,7 @@
     	</div>
         <div id="excel_data2" class="mt-5"></div>
         
+        @if(!isset($kiemtra))
         <div class="card mt-4">
     		<div class="card-body">
     			<div class="row">
@@ -104,6 +105,7 @@
     		</div>
     	</div>
         <div id="excel_data" class="mt-5"></div>
+        @endif
 
 
     </div>
@@ -127,6 +129,7 @@
 
 <script src="{{ asset('js/xlsx.full.min.js') }}"></script>
 <script>
+    @if(!isset($kiemtra))
     const excel_file = document.getElementById('excel_file');
     excel_file.addEventListener('change', (event) => {
         if(!['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'].includes(event.target.files[0].type))
@@ -169,8 +172,7 @@
             // excel_file.value = '';
         }
     });
-
-
+    @endif
 
     $("#selectYear").on("change", function() {
         if($(this).val() == ""){
